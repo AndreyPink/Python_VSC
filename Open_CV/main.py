f6b5b3400d75a10d3de98a790b0eaecc54b1652f
ANDREY PINK <andreypink@bk.ru>
2023-02-13 12:31:14 +0300

    update

---
+++ Open_CV/main.py
@@ -0,0 +1,11 @@
+# Тут мы рассматриваем пример увеличения фотографии или обьекта изображения.
+import cv2
+
+img = cv2.imread('kartinki-vyzdoravlivaj-1.jpg')
+print(img.shape)
+
+img = cv2.resize(img, (1000, 1000))
+print(img.shape)
+
+cv2.imshow('Result', img)
+cv2.waitKey(0)
