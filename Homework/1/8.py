d2a8648c66eb60c3ae6a130d903292ec74ba872c
ANDREY PINK <andreypink@bk.ru>
2023-02-17 13:35:01 +0300

    update

--- Homework/1/8.py
+++ Homework/1/8.py
@@ -5,10 +5,18 @@
 # 3 2 4 -> yes
 # 3 2 1 -> no
 
+# n = int(input('Введите кол-во долек по горизонтали: '))
+# m = int(input('Введите кол-во долек по вертикали: '))
+# k = int(input('Введите желаемое кол-во долек: '))
+# if k >= n and k / n == round(k / n) or k >= m and k / m == round(k / m):
+#     print('YES')
+# else:
+#     print('NO')
+    
 n = int(input('Введите кол-во долек по горизонтали: '))
 m = int(input('Введите кол-во долек по вертикали: '))
 k = int(input('Введите желаемое кол-во долек: '))
-if k >= n and k / n == round(k / n) or k >= m and k / m == round(k / m):
+if k % n == 0 and k <= m * n or k % m == 0 and k <= m * n:
     print('YES')
 else:
     print('NO')
