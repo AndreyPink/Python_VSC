50f959de6124f56c3ce44049112bc2466f7477ef
ANDREY PINK <andreypink@bk.ru>
2023-02-20 16:21:03 +0300

    update

--- Homework/2/10.py
+++ Homework/2/10.py
@@ -5,3 +5,17 @@
 # количество монет, которые нужно перевернуть.
 # 5 -> 1 0 1 1 0
 # 2
+
+number_coins = int(input('Input number of coins: '))
+side_1_count = 0
+side_0_count = 0
+for i in range(number_coins):
+    side = int(input(f'Input side of {i + 1} coin (1 or 0): '))
+    if side == 1:
+        side_1_count += 1
+    else:
+        side_0_count += 1
+if side_1_count < side_0_count:
+    print(f'Min revers = {side_1_count}')
+else:
+    print(f'Min revers = {side_0_count}')
