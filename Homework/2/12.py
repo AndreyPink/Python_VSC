57c5ef35a0c3df3ab7445f87fc8f79c8caed6fbc
ANDREY PINK <andreypink@bk.ru>
2023-02-21 01:57:31 +0300

    update

--- Homework/2/12.py
+++ Homework/2/12.py
@@ -5,3 +5,24 @@
 # произведение P. Помогите Кате отгадать задуманные Петей числа.
 # 4 4 -> 2 2
 # 5 6 -> 2 3
+
+"""
+import math
+sum = int(input('Input Sum of numbers: '))
+prod = int(input('Input Product of numbers: '))
+x = abs(-sum + math.sqrt(sum ** 2 - 4 * prod)) / 2
+if x % 1 != 0:
+    print('Input error')
+else:    
+    print(f'X = {round(x)}, Y = {sum - round(x)}')
+"""
+        
+sum = int(input('Input Sum of numbers: '))
+prod = int(input('Input Product of numbers: '))
+for i in range(1, 1001):
+    for j in range(1, 1001):
+        if sum == i + j and prod == i * j:
+            x, y = i, j
+print(f'X = {x}, Y = {y}')        
+
+
--- Homework/2/14.py
+++ Homework/2/14.py
@@ -1,3 +1,13 @@
 # Требуется вывести все целые степени двойки (т.е. числа
 # вида 2k), не превосходящие числа N.
 # 10 -> 1 2 4 8
+
+num = int(input('Input N: '))
+prod = 0
+degree = 0
+while prod * 2 < num:
+    prod = 2 ** degree
+    print(prod, end=' ')
+    degree += 1
+ 
+    
