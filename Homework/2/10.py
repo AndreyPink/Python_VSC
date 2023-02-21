99e8b6dcfcd9e35757e8c8b54d1dac148df3314c
ANDREY PINK <andreypink@bk.ru>
2023-02-21 11:52:03 +0300

    update

--- Homework/2/10.py
+++ Homework/2/10.py
@@ -6,11 +6,27 @@
 # 5 -> 1 0 1 1 0
 # 2
 
+# number_coins = int(input('Input number of coins: '))
+# side_1_count = 0
+# side_0_count = 0
+# for i in range(number_coins):
+#     side = int(input(f'Input side of {i + 1} coin (1 or 0): '))
+#     if side == 1:
+#         side_1_count += 1
+#     else:
+#         side_0_count += 1
+# if side_1_count < side_0_count:
+#     print(f'Min revers = {side_1_count}')
+# else:
+#     print(f'Min revers = {side_0_count}')
+    
+import random    
 number_coins = int(input('Input number of coins: '))
 side_1_count = 0
 side_0_count = 0
 for i in range(number_coins):
-    side = int(input(f'Input side of {i + 1} coin (1 or 0): '))
+    side = random.randint(0,1)
+    print(f'side of {i + 1} coin = {side}')
     if side == 1:
         side_1_count += 1
     else:
