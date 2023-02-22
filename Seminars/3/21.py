201d04a1c04ef08f13d166bc75611c9079152962
ANDREY PINK <andreypink@bk.ru>
2023-02-22 10:56:51 +0300

    update

--- Seminars/3/21.py
+++ Seminars/3/21.py
@@ -4,3 +4,11 @@
 # {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
 # ":" S007 "}]
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+my_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
+           {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
+new_list = []
+for elem in my_dict:
+    if elem.values  not in new_list:
+        new_list.append(elem.values())
+print(f'New list: {new_list}')
