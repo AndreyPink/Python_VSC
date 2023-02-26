34e8396a74d18bbedd598ae628729b49d49e51a2
ANDREY PINK <andreypink@bk.ru>
2023-02-26 21:43:15 +0300

    update

--- Seminars/4/25.py
+++ Seminars/4/25.py
@@ -5,7 +5,7 @@
 # Input: a a a b c a a d c d d
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 
-my_string = (input('Input string: ')).split()
+my_string = input('Input string: ').split()
 my_dict = {elem: 0 for elem in set(my_string)}
 for elem in my_string:
     my_dict[elem] += 1
