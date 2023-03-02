46bf30db6534adf471812afb56b27adb9890649e
ANDREY PINK <andreypink@bk.ru>
2023-03-02 15:06:18 +0300

    update

--- Seminars/5/31.py
+++ Seminars/5/31.py
@@ -5,3 +5,17 @@
 # Input: 7
 # Output: 21
 # Задание необходимо решать через рекурсию
+
+
+def fibo(number, count = 2, fibo_min_1 = 1, fibo_min_2 = 0):
+    if number == 0:
+        return 0
+    if number == 1:
+        return 1
+    if count == number:
+        return fibo_min_1 + fibo_min_2
+    else:
+        return fibo(number, count + 1, fibo_min_1 + fibo_min_2, fibo_min_1)
+num_fibo = int(input('Input N_fibo: '))
+print(f'Fibo_{num_fibo} =', fibo(num_fibo))
+    
