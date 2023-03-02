9bdeb452eff103a4504346559af8375033a71a94
ANDREY PINK <andreypink@bk.ru>
2023-03-02 19:11:22 +0300

    update

--- Homework/5/28.py
+++ Homework/5/28.py
@@ -6,11 +6,12 @@
 # 4
 
 def sum(a, b):
-    if b > 0:
-        a += 1
-        return sum(a, b-1)
-    else:
-        return a
+    # if b > 0:
+    #     a += 1
+    #     return sum(a, b-1)
+    # else:
+    #     return a
+    return sum(a+1, b-1) if b > 0 else a
 
 num_1 = int(input('Input num_1: '))    
 num_2 = int(input('Input num_2: '))   
--- Seminars/5/31.py
+++ Seminars/5/31.py
@@ -7,7 +7,7 @@
 # Задание необходимо решать через рекурсию
 
 
-def fibo(number, count = 2, fibo_min_1 = 1, fibo_min_2 = 0):
+def fibo(number, count = 2, fibo_min_1 = 1, fibo_min_2 = 0): 
     if number == 0:
         return 0
     if number == 1:
@@ -18,4 +18,5 @@ def fibo(number, count = 2, fibo_min_1 = 1, fibo_min_2 = 0):
         return fibo(number, count + 1, fibo_min_1 + fibo_min_2, fibo_min_1)
 num_fibo = int(input('Input N_fibo: '))
 print(f'Fibo_{num_fibo} =', fibo(num_fibo))
+
     
