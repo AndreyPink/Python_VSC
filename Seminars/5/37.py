8d5dd1f86de161eb4cd88964ffc74a2c9a3c558f
ANDREY PINK <andreypink@bk.ru>
2023-03-02 17:19:02 +0300

    update

--- Seminars/5/37.py
+++ Seminars/5/37.py
@@ -4,3 +4,24 @@
 # (даже для ввода и вывода).
 # Input: 2 -> 3 4
 # Output: 4 3
+
+
+# import random
+# def print_list(num, count = 0, my_list = ''):
+#     if count < num:
+#         return print_list(num, count + 1, my_list + ' ' + str(random.randint(1,9)))
+#     print(f'Original list:{my_list}')
+#     return my_list[::-1]
+    
+# number = int(input('Input length of list: '))    
+# print(f'Reverse list: {print_list(number)}')
+
+import random
+def print_list(num, count = 0, my_list = ''):
+    if count < num:
+        return print_list(num, count + 1, my_list + ' ' + str(random.randint(1,9)))
+    print(f'Original list:{my_list}')
+    return my_list[::-1]
+    
+number = int(input('Input length of list: '))    
+print(f'Reverse list: {print_list(number)}')
