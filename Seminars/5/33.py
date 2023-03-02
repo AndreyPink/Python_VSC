cc7ed9a7f2902f795e38bd2eea6e702467d0628a
ANDREY PINK <andreypink@bk.ru>
2023-03-02 15:22:16 +0300

    update

--- Seminars/5/33.py
+++ Seminars/5/33.py
@@ -5,3 +5,13 @@
 # максимальные – на минимальные.
 # Input: 5 -> 1 3 3 3 4
 # Output: 1 3 3 3 1
+
+import random
+def change_grade(grade_list):
+    return str(grade_list).replace(str(max(grade_list)), str(min(grade_list)))
+
+amount = int(input('Input amount of grades: '))
+grade_list = [random.randint(1,5) for grade in range(amount)]
+
+print(grade_list)
+print(change_grade(grade_list))
