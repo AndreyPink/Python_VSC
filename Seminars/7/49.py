7586344cd108032f0cf0447c232c222018d8d1c7
ANDREY PINK <andreypink@bk.ru>
2023-03-07 16:37:25 +0300

    update

--- Seminars/7/49.py
+++ Seminars/7/49.py
@@ -21,3 +21,9 @@
 # print(*find_farthest_orbit(orbits))
 # Вывод:
 # 2.5 10
+
+
+def find_farthest_orbit(list_of_orbits):
+    return max(list_of_orbits, key= lambda x: max(x))
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+print(*find_farthest_orbit(orbits))
