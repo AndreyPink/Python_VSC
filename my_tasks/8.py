e1e52ee8953042641657126346e57fd9d5293d84
ANDREY PINK <andreypink@bk.ru>
2023-03-12 19:27:50 +0300

    update

--- my_tasks/8.py
+++ my_tasks/8.py
@@ -1,5 +1,8 @@
 # Напишите программу, которая принимает имя файла и выводит его расширение. 
 # Если расширение у файла определить невозможно, выбросите исключение.
 
-file_name = 'info.txt'.split('.')
-print()
+file_name = 'info.txt'
+if '.' not in file_name or file_name[-1] == '.':
+    raise ValueError('the file has no extension')
+file_name = file_name.split('.')
+print(f'Ext - {file_name[-1]}')
