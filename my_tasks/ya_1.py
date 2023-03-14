266d66b2978095f6013659efd693a68244be2266
ANDREY PINK <andreypink@bk.ru>
2023-03-14 13:50:24 +0300

    update

--- my_tasks/ya_1.py
+++ my_tasks/ya_1.py
@@ -1,13 +1,22 @@
 from collections import Counter
 
 text = 'hellohfyfkgkhjlhllkuiyyuo8uo'
+my_string = ''
+result_string = ''
+full_list = []
 my_dict = dict(Counter(text))
-print(*my_dict.keys())
+
 for _ in range(max(my_dict.values())):
     my_list = ['#' if value > 0 else ' ' for key, value in my_dict.items()]
-    print(*my_list)
     for key, value in my_dict.items():
         my_dict[key] = value - 1
+    full_list.append(my_list)    
+for my_list in reversed(full_list):
+    for i in my_list:
+        my_string += i + ' '
+    result_string += my_string +'\n'
+    my_string = ''
         
+print(result_string)
+print(*my_dict.keys())
         
-    # my_list = [(my_dict[key] = value - 1) for key, value in my_dict.items()]
