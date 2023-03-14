0fb5b9e52024c45bcef2886a168c0fa979cf5b1e
ANDREY PINK <andreypink@bk.ru>
2023-03-14 12:58:40 +0300

    update

--- my_tasks/20.py
+++ my_tasks/20.py
@@ -1,8 +1,13 @@
 from collections import Counter
 
-text = 'hello'
+text = 'hellohfyfkgkhjlhllkuiyyuo8uo'
 my_dict = dict(Counter(text))
-for _ in max(my_dict.values()):
+print(*my_dict.keys())
+for _ in range(max(my_dict.values())):
     my_list = ['#' if value > 0 else ' ' for key, value in my_dict.items()]
-    print(my_list)
-    my_list = [(my_dict[key] = value - 1) for key, value in my_dict.items()]
+    print(*my_list)
+    for key, value in my_dict.items():
+        my_dict[key] = value - 1
+        
+        
+    # my_list = [(my_dict[key] = value - 1) for key, value in my_dict.items()]
