a43efff819d939f64580d430b6c500e9828adf35
ANDREY PINK <andreypink@bk.ru>
2023-03-16 12:49:02 +0300

    update

---
+++ Seminars/9/57.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv('Seminars/9/california_housing_train.csv')
+
+# df.head()
+
+# df.tail()
+
+# df.shape()
+
+# df.isnull()
+
+# df.isnull().sum()
+
+# df.dtypes
+
+# df.columns
